6af505d9e88bd27fde130489e9e15ed2e069ae45
Clément Baziret <clement.baziret@epitech.eu>
2026-05-02 13:39:16 +0200

    :sparkles: Add advanced sessions plugins

---
+++ Source/TanknRoll/TanknRoll.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TanknRoll.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TanknRoll, "TanknRoll" );
